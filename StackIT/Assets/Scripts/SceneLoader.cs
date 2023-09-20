b16d54393d7b7151b3c53563d5d91a91ced128ac
Kurt Guen Petrola <kurtguenpetrola2.0@gmail.com>
2023-09-20 10:28:14 +0800

    added sceneloader delay and fixed prefabs

--- StackIT/Assets/Scripts/SceneLoader.cs
+++ StackIT/Assets/Scripts/SceneLoader.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-
     public GameObject loadingScreen;
     public Slider slider;
     public Text progressText;
+    public float delayBeforeStart = 1.0f; // Delay before starting the loading
+    public float progressMultiplier = 0.1f; // Adjust this to control the progress speed
 
     public void LoadScene(int sceneIndex)
     {
@@ -17,6 +18,9 @@ public class SceneLoader : MonoBehaviour
 
     IEnumerator LoadAsynchronously(int sceneIndex)
     {
+        // Delay for a specified time before showing the loading screen
+        yield return new WaitForSeconds(delayBeforeStart);
+
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
         loadingScreen.SetActive(true);
@@ -25,11 +29,11 @@ public class SceneLoader : MonoBehaviour
         {
             float progress = Mathf.Clamp01(operation.progress / .9f);
 
-            slider.value = progress;
-            progressText.text = progress * 100f + "%";
+            // Slow down the progress by multiplying it by a smaller factor
+            slider.value += progress * progressMultiplier;
+            progressText.text = (slider.value * 100f).ToString("F0") + "%";
 
             yield return null;
         }
-
     }
 }
