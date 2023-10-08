82292429470ca031bbcd3767b52c10b10cce63ea
jhonemil <142031916+jhonemil@users.noreply.github.com>
2023-10-08 21:39:19 +0800

    sound on/off
    sound on/off done

--- StackIT/Assets/Scripts/AudioManager.cs
+++ StackIT/Assets/Scripts/AudioManager.cs
@@ -1,31 +1,35 @@
-using UnityEngine.Audio;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
+    public Button muteButton;    // Reference to the "Mute" button.
+    public Button unmuteButton;  // Reference to the "Unmute" button.
+
+    private bool isMuted = false;
 
     public static AudioManager instance;
-    // Start is called before the first frame update
+
     void Awake()
     {
-
-        //if (instance == null)
-            //instance = this;
-        //else
-        //{
-            //Destroy(gameObject);
-            //return;
-        //}
-
-        //DontDestroyOnLoad(gameObject);
+        // Singleton pattern for AudioManager
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
 
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
-
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
@@ -35,8 +39,10 @@ public class AudioManager : MonoBehaviour
     private void Start()
     {
         Play("Theme-Music");
+        UpdateButtonVisibility();
     }
-    public void Play (string name)
+
+    public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if (s == null)
@@ -44,12 +50,23 @@ public class AudioManager : MonoBehaviour
             Debug.LogWarning("Sound: " + name + " not found!");
             return;
         }
-            
         s.source.Play();
     }
-    // Update is called once per frame
-    void Update()
+
+    public void ToggleMute()
     {
-        
+        isMuted = !isMuted;
+
+        // Toggle button visibility
+        muteButton.gameObject.SetActive(isMuted);
+        unmuteButton.gameObject.SetActive(!isMuted);
+        Debug.Log("Mute State: " + isMuted);
+    }
+
+    private void UpdateButtonVisibility()
+    {
+        // Set initial button visibility based on the mute state
+        muteButton.gameObject.SetActive(!isMuted);
+        unmuteButton.gameObject.SetActive(isMuted);
     }
 }
