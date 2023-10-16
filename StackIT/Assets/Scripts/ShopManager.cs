a48b9074ea859a45ef8a63c8b49bec939c91ddb4
jhonemil <142031916+jhonemil@users.noreply.github.com>
2023-10-16 20:21:32 +0800

    highest score
    highest score done

--- StackIT/Assets/Scripts/ShopManager.cs
+++ StackIT/Assets/Scripts/ShopManager.cs
@@ -1,23 +1,50 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    public Image lockIcon2xItem;
+    public GameObject item2x; // Reference to the 2x item GameObject
 
-    private bool is2xItemUnlocked = false;
+    private static ShopManager instance;
 
+    public static ShopManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<ShopManager>();
+            }
+            return instance;
+        }
+    }
+
+    public bool is2xItemUnlocked { get; private set; }
+
+    // Call this method to unlock the 2x item
     public void Unlock2xItem()
     {
         is2xItemUnlocked = true;
-        UpdateShopUI();
+
+        // Activate the 2x item in your game, for example:
+        if (item2x != null)
+        {
+            item2x.SetActive(true);
+        }
     }
 
-    private void UpdateShopUI()
+    // Other methods for your shop functionality can be added here.
+
+    private void Awake()
     {
-        if (is2xItemUnlocked)
+        // Ensure there's only one instance of ShopManager
+        if (instance != null && instance != this)
         {
-            lockIcon2xItem.enabled = false; // Disable the lock icon
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject); // Keep the ShopManager between scenes if needed.
         }
     }
 }
