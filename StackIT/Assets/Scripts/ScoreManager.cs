575a9f05ef94894ce8fd3a8ed257a291d56195d9
jhonemil <142031916+jhonemil@users.noreply.github.com>
2023-10-16 20:22:39 +0800

    highest score
    highest score done

--- StackIT/Assets/Scripts/ScoreManager.cs
+++ StackIT/Assets/Scripts/ScoreManager.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class ScoreManager : MonoBehaviour
 {
     public Text scoreText;
+<<<<<<< Updated upstream
     public Text highestScoreText;
     public Text unlockText;
 
@@ -18,6 +19,14 @@ public class ScoreManager : MonoBehaviour
         LoadHighestScore();
         UpdateHighestScoreUI();
     }
+=======
+    public Text unlockText; // Reference to the unlock message text element
+    private int playerScore = 0;
+    private int stackedItems = 0; // Track the number of stacked items
+    private bool isUnlockMessageShowing = false;
+
+    public ShopManager shopManager; // Reference to the ShopManager in the same scene
+>>>>>>> Stashed changes
 
     public void IncreaseScore()
     {
@@ -47,6 +56,7 @@ public class ScoreManager : MonoBehaviour
         scoreText.text = "Score: " + playerScore.ToString();
     }
 
+<<<<<<< Updated upstream
     private void LoadHighestScore()
     {
         highestScore = PlayerPrefs.GetInt("HighestScore", 0);
@@ -61,6 +71,19 @@ public class ScoreManager : MonoBehaviour
     private void UpdateHighestScoreUI()
     {
         highestScoreText.text = "Highest Score: " + highestScore.ToString();
+=======
+        // Check if the player has successfully stacked 5 items
+        if (playerScore == 3 && !isUnlockMessageShowing)
+        {
+            StartCoroutine(ShowUnlockMessage());
+
+            // Unlock the 2x item in the shop
+            if (shopManager != null)
+            {
+                shopManager.Unlock2xItem();
+            }
+        }
+>>>>>>> Stashed changes
     }
 
     private IEnumerator ShowUnlockMessage()
@@ -68,9 +91,14 @@ public class ScoreManager : MonoBehaviour
         isUnlockMessageShowing = true;
         unlockText.text = "2X Activated";
         unlockText.gameObject.SetActive(true);
+<<<<<<< Updated upstream
 
         yield return new WaitForSeconds(1f);
+=======
+        yield return new WaitForSeconds(5f); // Display the message for 5 seconds
+>>>>>>> Stashed changes
         unlockText.gameObject.SetActive(false);
         isUnlockMessageShowing = false;
     }
 }
+
